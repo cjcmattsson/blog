7bed3b75b307c66c8c68079ebdee436ea1ba2de2
cjcmattsson <cjcmattsson@gmail.com>
2018-04-03 13:05:10 +0200

    Worked during school

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Illuminate\Routing\Controller;
+use Illuminate\View\View;
+use App\User;
 
 class PagesController extends Controller
 {
-  public function index ()
+  public function index (): View
   {
-        return view('index', ['name' => 'Laravel']);
+
+    return view('index', ['name' => 'Laravel']);
 
   }
 }
